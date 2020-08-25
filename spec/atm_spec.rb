322edf2c5a381b1d5eb01982720b485a75f1c26d
SebastianN97 <sebbe132132@gmail.com>
2020-08-25 09:39:57 +0200

    Defining instance double into the file atm_spec.rb and naming it account. Giving account balance of 100. Adding before do on line 7 to 13

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,17 @@
 
 require './lib/atm.rb'
 describe Atm do
+    let(:account) { instance_double('Account')}
+
+    before do
+        # Before each test we need to add an attribute of `balance`
+        # to the `account` object and set the value to `100`
+        allow(account).to receive(:balance).and_return(100)
+        # We also need to allow `account` to receive the new balance
+        # using the setter method `balance=`
+        allow(account).to receive(:balance=)
+    end
+
     it 'has 1000$ on initialize' do
         expect(subject.funds).to eq 1000
     end

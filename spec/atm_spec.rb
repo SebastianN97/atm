4a76446134e559341b6c51e0ee951bc5bf871b50
sara <saralundkvist86@gmail.com>
2020-08-25 09:52:28 +0200

    whriting new test, it 'allow withdraw if the account has enough balance' in atm_spec.rb:24

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -20,6 +20,13 @@ describe Atm do
         subject.withdraw 50
         expect(subject.funds).to eq 950
     end
+    
+    it 'allow withdraw if the account has enough balance.' do
+        expected_output = { status: true, message: 'success', Date.today, amount: 45}
+        expect(subject.withdraw(45,account)).to. eq expected_output
+    end
 end
+
+
 
 
